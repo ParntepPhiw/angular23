e108ab73efdabf66376fbd08a120dde8d0b062b1
ParntepPhiw <parntep.etc@gmail.com>
2021-03-03 20:30:45 +0700

    Update backend.service.ts

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -5,11 +5,25 @@ import { Product } from './product';
   providedIn: 'root'
 })
 export class BackendService {
-
+  products: Product[];
+  
   constructor() { }
 
   getProducts() : Product[] {
-    return [];
+    this.products = [];
+    this.products.push({
+      name: 'ส้มโอ',
+      price: 111
+    });
+    this.products.push({
+      name: 'แตงโม',
+      price: 222
+    });
+    this.products.push({
+      name: 'มะพร้าวน้ำหอม',
+      price: 333
+    });
+    return this.products;
   }
 
   getProductById(productId: number) : Product {
